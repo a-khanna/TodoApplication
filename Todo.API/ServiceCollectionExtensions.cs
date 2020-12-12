e502e32ab7c5b13a7ec0fd6e1bc05fb66f634c1a
anirudhkhanna <anirudh.khanna@nagarro.com>
2020-12-12 12:59:03 +0530

    Remove padlock icons from user controller on swaggerUI

--- Todo.API/ServiceCollectionExtensions.cs
+++ Todo.API/ServiceCollectionExtensions.cs
@@ -51,7 +51,13 @@ namespace Todo.API
                     Title = "Todo API",
                     Description = "API for managing Todo lists and items"
                 });
+
+                // Examples for requests
                 c.SchemaFilter<ExampleSchemaFilter>();
+
+                // Authorization requirement
+                c.OperationFilter<AuthorizeOperationFilter>();
+
                 // To Enable authorization using Swagger (JWT)  
                 c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
                 {
@@ -61,20 +67,6 @@ namespace Todo.API
                     BearerFormat = "JWT",
                     In = ParameterLocation.Header,
                     Description = "JWT Authorization header using the Bearer scheme. \r\n\r\n Enter 'Bearer' [space] and then your token in the text input below.\r\n\r\nExample: \"Bearer 12345abcdef\"",
-                });
-                c.AddSecurityRequirement(new OpenApiSecurityRequirement
-                {
-                    {
-                        new OpenApiSecurityScheme
-                        {
-                            Reference = new OpenApiReference
-                            {
-                                Type = ReferenceType.SecurityScheme,
-                                Id = "Bearer"
-                            }
-                        },
-                        new string[] {}
-                    }
                 });
 
                 // Set the comments path for the Swagger JSON and UI.
